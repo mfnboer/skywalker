6ae71cd40dba156a363ce984866b9c74e953f18e
Michel de Boer <michelboer@xs4all.nl>
2024-01-03 12:26:53 +0100

    Replace PickVisualMedia by GetContent for photo picking

--- android/src/com/gmail/mfnboer/QPhotoPicker.java
+++ android/src/com/gmail/mfnboer/QPhotoPicker.java
@@ -8,13 +8,16 @@ import org.qtproject.qt.android.QtNative;
 import com.gmail.mfnboer.FileUtils;
 
 import java.lang.String;
-import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia;
+//import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia;
+import androidx.activity.result.contract.ActivityResultContracts.GetContent;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.PickVisualMediaRequest;
+//import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultCaller;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -35,26 +38,55 @@ public class QPhotoPicker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(LOGTAG, "onCreate");
 
-        // Registers a photo picker activity launcher in single-select mode.
-        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
-                registerForActivityResult(new PickVisualMedia(), uri -> {
-            // Callback is invoked after the user selects a media item or closes the
-            // photo picker.
-            if (uri != null) {
-                Log.d("PhotoPicker", "Selected URI: " + uri);
-                int fd = FileUtils.openContentUri(uri);
-                emitPhotoPicked(fd);
-            } else {
-                Log.d("PhotoPicker", "No media selected");
-                emitPhotoPickCanceled();
-            }
+        ActivityResultLauncher<String> getContent = registerForActivityResult(new GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                        int fd = FileUtils.openContentUri(uri);
+                        emitPhotoPicked(fd);
+                    } else {
+                        Log.d("PhotoPicker", "No media selected");
+                        emitPhotoPickCanceled();
+                    }
 
-            finish();
+                    finish();
+                }
         });
 
-        // Launch the photo picker and let the user choose images and videos.
-        pickMedia.launch(new PickVisualMediaRequest.Builder()
-                .setMediaType(PickVisualMedia.ImageOnly.INSTANCE)
-                .build());
+        getContent.launch("image/*");
     }
+
+    // PickVisualMediaRequest should be the way to go according to the Android documentation.
+    // However, this has a serious permission limits which makes almost all photo albums
+    // inaccessible to the user. Maybe it will be better in Android 14. Till then we
+    // use GetContent instead.
+    //
+    // public void onCreate(Bundle savedInstanceState) {
+    //     super.onCreate(savedInstanceState);
+    //     Log.d(LOGTAG, "onCreate");
+
+    //     // Registers a photo picker activity launcher in single-select mode.
+    //     ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+    //             registerForActivityResult(new PickVisualMedia(), uri -> {
+    //         // Callback is invoked after the user selects a media item or closes the
+    //         // photo picker.
+    //         if (uri != null) {
+    //             Log.d("PhotoPicker", "Selected URI: " + uri);
+    //             int fd = FileUtils.openContentUri(uri);
+    //             emitPhotoPicked(fd);
+    //         } else {
+    //             Log.d("PhotoPicker", "No media selected");
+    //             emitPhotoPickCanceled();
+    //         }
+
+    //         finish();
+    //     });
+
+    //     // Launch the photo picker and let the user choose images and videos.
+    //     pickMedia.launch(new PickVisualMediaRequest.Builder()
+    //             .setMediaType(PickVisualMedia.ImageOnly.INSTANCE)
+    //             .build());
+    // }
 }
